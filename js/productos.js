cfeacf5158bf987aae7912344d10de4ae876a76e
nexocuba <nexocuba2026@gmail.com>
2026-03-11 14:43:00 -0400

    Crear js/productos.js

---
+++ js/productos.js
@@ -0,0 +1,21 @@
+import { supabase } from "./supabase.js"
+
+export async function obtenerProductos(){
+
+const { data, error } = await supabase
+
+.from("productos")
+
+.select("*")
+
+if(error){
+
+console.log(error)
+
+return []
+
+}
+
+return data
+
+}
