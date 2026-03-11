3a7052c692e6f579ef68c1929a0ff993b5c040e4
nexocuba <nexocuba2026@gmail.com>
2026-03-11 14:44:09 -0400

    Crear js/proveedores.js

---
+++ js/proveedores.js
@@ -0,0 +1,41 @@
+import { supabase } from "./supabase.js"
+
+const container = document.getElementById("proveedoresContainer")
+
+async function cargarProveedores(){
+
+const { data } = await supabase
+
+.from("proveedores")
+
+.select("*")
+
+container.innerHTML=""
+
+data.forEach(p=>{
+
+const div = document.createElement("div")
+
+div.className="producto"
+
+div.innerHTML=`
+
+<h3>${p.nombre}</h3>
+
+<p>NIT: ${p.nit}</p>
+
+<p>Cuenta: ${p.cuenta_bancaria}</p>
+
+<p>Dirección: ${p.direccion}</p>
+
+<p>Contrato: ${p.contrato}</p>
+
+`
+
+container.appendChild(div)
+
+})
+
+}
+
+cargarProveedores()
