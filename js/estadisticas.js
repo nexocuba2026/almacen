12da070c114d259fb37a049c4eb90e8d774b73f6
nexocuba <nexocuba2026@gmail.com>
2026-03-11 14:44:30 -0400

    Crear js/estadisticas.js

---
+++ js/estadisticas.js
@@ -0,0 +1,55 @@
+import { supabase } from "./supabase.js"
+
+const container = document.getElementById("statsContainer")
+
+async function cargarStats(){
+
+const { data } = await supabase
+
+.from("productos")
+
+.select("*")
+
+let totalStock = 0
+let totalEntradas = 0
+let totalSalidas = 0
+
+data.forEach(p=>{
+
+totalStock += p.stock
+totalEntradas += p.entradas
+totalSalidas += p.salidas
+
+})
+
+container.innerHTML = `
+
+<div class="producto">
+
+<h3>Stock total</h3>
+
+<p>${totalStock}</p>
+
+</div>
+
+<div class="producto">
+
+<h3>Entradas totales</h3>
+
+<p>${totalEntradas}</p>
+
+</div>
+
+<div class="producto">
+
+<h3>Salidas totales</h3>
+
+<p>${totalSalidas}</p>
+
+</div>
+
+`
+
+}
+
+cargarStats()
