c06bdf36214606d3251b9bebc02254272fb6988b
nexocuba <nexocuba2026@gmail.com>
2026-03-12 13:23:03 -0400

    Actualizar loadComponents.js

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -1,14 +1,9 @@
 export async function cargarComponentes(){
+  const header = await fetch("components/header.html")
+  const headerHTML = await header.text()
+  document.getElementById("header").innerHTML = headerHTML
 
-const header = await fetch("components/header.html")
-const headerHTML = await header.text()
-
-document.getElementById("header").innerHTML = headerHTML
-
-
-const footer = await fetch("components/footer.html")
-const footerHTML = await footer.text()
-
-document.getElementById("footer").innerHTML = footerHTML
-
+  const footer = await fetch("components/footer.html")
+  const footerHTML = await footer.text()
+  document.getElementById("footer").innerHTML = footerHTML
 }
