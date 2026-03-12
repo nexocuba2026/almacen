095501f4c4a0a033ea8b9b317abbbf36e31230ac
nexocuba <nexocuba2026@gmail.com>
2026-03-12 13:21:26 -0400

    Actualizar productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,12 @@
 import { supabase } from "./supabase.js"
 
 export async function obtenerProductos(){
-
-const { data, error } = await supabase
-
-.from("productos")
-
-.select("*")
-
-if(error){
-
-console.log(error)
-
-return []
-
-}
-
-return data
-
+  const { data, error } = await supabase
+    .from("productos")
+    .select("*")
+  if(error){
+    console.log(error)
+    return []
+  }
+  return data
 }
