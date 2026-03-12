276eab5d32377e732dd64c6f74b94a17c958c069
nexocuba <nexocuba2026@gmail.com>
2026-03-12 13:20:58 -0400

    Actualizar supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const supabaseUrl = "TU_URL_SUPABASE"
-const supabaseKey = "TU_ANON_KEY"
+const supabaseUrl = "https://yefamhdggbaoqtzemptg.supabase.co"
+const supabaseKey = "sb_publishable_YIhV-2Y214lyl3YRl4GBtg_E1do7qwX"
 
 export const supabase = createClient(
-supabaseUrl,
-supabaseKey
+  supabaseUrl,
+  supabaseKey
 )
